034bd289a1cc0057826592906a39f717b455c6b9
vpuzyrevich <v.puzyrevich@gmail.com>
2022-04-29 22:39:28 +0300

    task repeater and recursive-depth

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  let string;
+  let additionStr;
+  let additionArr = [];
+  let additionRes;
+  if(typeof str !== 'string') {
+      string = String(str);
+  } else {
+      string = str;
+  }
+  if(options.addition === undefined) {
+      options.addition = '';
+  }
+  if(options.addition !== undefined && typeof options.addition !== 'string') {
+      additionStr = String(options.addition);
+  } else {
+      additionStr = options.addition;
+  }
+  if(options.separator === undefined) {
+      options.separator = '+';
+  }
+  if(options.additionSeparator === undefined) {
+      options.additionSeparator = '|';
+  }
+  if(options.additionRepeatTimes !== undefined) {
+      for(let i =0; i < options.additionRepeatTimes; i++) {
+          additionArr.push(`${additionStr}`);
+          additionArr.push(`${options.additionSeparator}`);
+      }
+      additionArr.pop();
+      additionRes = additionArr.join('');
+  } else {
+      additionRes = `${additionStr}`;
+  }
+  let arr = [];
+  let result;
+  if(options.repeatTimes !== undefined) {
+      for(let i =0; i < options.repeatTimes; i++) {
+          arr.push(`${string}${additionRes}`);
+          arr.push(`${options.separator}`);
+      }
+      arr.pop();
+      result = arr.join('');
+  } else {
+      result = `${string}${additionRes}`;
+  }
+  return result;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,10 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class DepthCalculator {
+  calculateDepth(arr) {
+      let count = 0;
+      for(let i = 0; i < arr.length; i++) {
+          if(Array.isArray(arr[i])) {
+              count = Math.max(count, this.calculateDepth(arr[i]));
+          }
+      }
+      return count + 1;
   }
 }
 
