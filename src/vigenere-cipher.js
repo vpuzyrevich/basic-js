cfd5925696f6211443f53c2d8233aed11f0397e0
vpuzyrevich <v.puzyrevich@gmail.com>
2022-05-01 11:22:24 +0300

    task VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(bool) {
+      this.bool = bool;
+      this.result = [];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    let result = [];
+    if(message === undefined || key === undefined) {
+        throw new Error('Incorrect arguments!');
+    }
+    // const re = /[A-Z]/g;
+    const lat = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let messageUp = message.toUpperCase();
+    let keyUp = key.toUpperCase();
+    
+    let k = Math.ceil(messageUp.length / keyUp.length);
+    keyUp = keyUp.repeat(k);
+    // let messageArr = messageUp.split('');
+    let start = 'A'.charCodeAt(0);
+    let abcCount = 26;
+    let keyArr = keyUp.split('');
+
+    for (let i = 0; i < messageUp.length; i++) {
+        if (lat.includes(messageUp[i])) {
+            let letterIdx = messageUp.charCodeAt(i) - start;
+            let shift = keyUp.charCodeAt(i) - start;
+            result.push(
+                String.fromCharCode(start + (letterIdx + shift) % abcCount)
+            );
+            
+        } else {
+            result.push(messageUp[i]);
+            keyArr.splice(i, 0, messageUp[i]);
+            keyUp = keyArr.join('');
+
+        }
+    }
+    if(this.bool === true || this.bool === undefined) {
+      return result.join('');
+  } else {
+      return result.reverse().join('');
   }
+}
+decrypt(message, key) {
+    let result = [];
+    if(message === undefined || key === undefined) {
+        throw new Error('Incorrect arguments!');
+    }
+    const lat = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let messageUp = message.toUpperCase();
+    let keyUp = key.toUpperCase();
+    
+    let k = Math.ceil(messageUp.length / keyUp.length);
+    keyUp = keyUp.repeat(k);
+    // let messageArr = messageUp.split('');
+    let start = 'A'.charCodeAt(0);
+    let abcCount = 26;
+    let keyArr = keyUp.split('');
+
+    for (let i = 0; i < messageUp.length; i++) {
+        if (lat.includes(messageUp[i])) {
+            let letterIdx = messageUp.charCodeAt(i) - start;
+            let shift = keyUp.charCodeAt(i) - start;
+            result.push(
+                String.fromCharCode(start + (letterIdx - shift + abcCount) % abcCount)
+            );
+            
+        } else {
+            result.push(messageUp[i]);
+            keyArr.splice(i, 0, messageUp[i]);
+            keyUp = keyArr.join('');
+
+        }
+    }
+    if(this.bool === true || this.bool === undefined) {
+        return result.join('');
+    } else {
+        return result.reverse().join('');
+    } 
+}
 }
 
 module.exports = {
